7b1b9a973b83edba12330dc5d4a756a9d4572465
fmrico <fmrico@users.noreply.github.com>
2025-09-19 07:50:42 +0000

    deploy: 530a4cfac4d71821b704571a5905351d79a3101e

--- search/functions_b.js
+++ search/functions_b.js
@@ -4,5 +4,6 @@ var searchData=
   ['navcel_5fcentroid_1',['navcel_centroid',['../classnavmap_1_1NavMap.html#a23220b7bea1310d9853ca902b3576633',1,'navmap::NavMap::navcel_centroid()'],['../classNavMap.html#a23220b7bea1310d9853ca902b3576633',1,'NavMap::navcel_centroid()']]],
   ['navcel_5fneighbors_2',['navcel_neighbors',['../classnavmap_1_1NavMap.html#a80833da2ad4b2cc73a5c2842fcff58a0',1,'navmap::NavMap::navcel_neighbors()'],['../classNavMap.html#a80833da2ad4b2cc73a5c2842fcff58a0',1,'NavMap::navcel_neighbors()']]],
   ['navcel_5fvalue_3',['navcel_value',['../classnavmap_1_1NavMap.html#ab001fd3c574363e6650de13813784c3d',1,'navmap::NavMap::navcel_value()'],['../classNavMap.html#ab001fd3c574363e6650de13813784c3d',1,'NavMap::navcel_value()']]],
-  ['navmapdisplay_4',['NavMapDisplay',['../classnavmap__rviz__plugin_1_1NavMapDisplay.html#a8d0048cf38eb3250489c97fd4359cd23',1,'navmap_rviz_plugin::NavMapDisplay']]]
+  ['navmap_4',['NavMap',['../classnavmap_1_1NavMap.html#ada6881dcd6865fb03a053244aa5f0e18',1,'navmap::NavMap::NavMap()'],['../classnavmap_1_1NavMap.html#a9fa159b44d4d690aca1f65b3e3a4ffe7',1,'navmap::NavMap::NavMap(const NavMap &amp;other)'],['../classNavMap.html#ada6881dcd6865fb03a053244aa5f0e18',1,'NavMap::NavMap()'],['../classNavMap.html#a9fa159b44d4d690aca1f65b3e3a4ffe7',1,'NavMap::NavMap(const NavMap &amp;other)']]],
+  ['navmapdisplay_5',['NavMapDisplay',['../classnavmap__rviz__plugin_1_1NavMapDisplay.html#a8d0048cf38eb3250489c97fd4359cd23',1,'navmap_rviz_plugin::NavMapDisplay']]]
 ];
