7fe0cb840dfb1ac1bddb1ca40dc432b894dd6c44
Francisco Martín Rico <fmrico@gmail.com>
2025-09-20 18:00:38 +0200

    Change License to Apache 2
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- navmap_rviz_plugin/include/navmap_rviz_plugin/NavMapDisplay.hpp
+++ navmap_rviz_plugin/include/navmap_rviz_plugin/NavMapDisplay.hpp
@@ -1,7 +1,18 @@
 // Copyright 2025 Intelligent Robotics Lab
 //
-// This file is part of Easy Navigation (EasyNav) under GPLv3.
-// See <http://www.gnu.org/licenses/> for details.
+// This file is part of the project Easy Navigation (EasyNav in short)
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #ifndef NAVMAP_RVIZ_PLUGIN__NAVMAP_DISPLAY_HPP_
 #define NAVMAP_RVIZ_PLUGIN__NAVMAP_DISPLAY_HPP_
