6b6484b881052ab82b9aa88a0152ce4253d63229
fmrico <fmrico@users.noreply.github.com>
2025-09-23 09:42:24 +0000

    deploy: b349fa9c575160a1cdeebc55d449d4bd12e23d96

--- search/functions_5.js
+++ search/functions_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['from_5fmsg_0',['from_msg',['../namespacenavmap__ros.html#ac70f12eb1f059a42b6993cb612720ea0',1,'navmap_ros::from_msg(const navmap_ros_interfaces::msg::NavMap &amp;msg)'],['../namespacenavmap__ros.html#a570457f653972601dcd8b1fa821af02a',1,'navmap_ros::from_msg(const navmap_ros_interfaces::msg::NavMapLayer &amp;msg, navmap::NavMap &amp;nm)'],['../namespacenavmap__ros.html#a9835bd4485202df104ab38cb5cdc1ed8',1,'navmap_ros::from_msg(const NavMap &amp;msg)']]],
   ['from_5foccupancy_5fgrid_1',['from_occupancy_grid',['../namespacenavmap__ros.html#af26f003771b7de20efd8c9d2c3c519c8',1,'navmap_ros::from_occupancy_grid()'],['../test__conversions_8cpp.html#af26f003771b7de20efd8c9d2c3c519c8',1,'from_occupancy_grid():&#160;conversions.cpp']]],
-  ['from_5fpointcloud2_2',['from_pointcloud2',['../namespacenavmap__ros.html#af1d0f450cee491ef385215a317c6876a',1,'navmap_ros']]]
+  ['from_5fpointcloud2_2',['from_pointcloud2',['../namespacenavmap__ros.html#af1d0f450cee491ef385215a317c6876a',1,'navmap_ros']]],
+  ['from_5fpoints_3',['from_points',['../namespacenavmap__ros.html#a731ad13a79c7d061fba796add4aea852',1,'navmap_ros']]]
 ];
