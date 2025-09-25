3ecc8921c81c406aff2e13195f4786b3dd92e1b3
fmrico <fmrico@users.noreply.github.com>
2025-09-25 17:55:06 +0000

    deploy: 24be6e8e6256ba682f73ec8c16fb2871229562f2

--- search/all_14.js
+++ search/all_14.js
@@ -6,12 +6,13 @@ var searchData=
   ['mark_5fvertex_5fupdated_3',['mark_vertex_updated',['../classnavmap_1_1NavMap.html#aa72881f1ddcdc3c30ce010982d642a7e',1,'navmap::NavMap::mark_vertex_updated()'],['../classNavMap.html#aa72881f1ddcdc3c30ce010982d642a7e',1,'NavMap::mark_vertex_updated()']]],
   ['marking_20areas_20tt_20set_5farea_20tt_4',['7. Marking areas (&lt;tt&gt;set_area&lt;/tt&gt;)',['../index.html#autotoc_md24',1,'']]],
   ['max_5',['max',['../structnavmap_1_1AABB.html#a06dabf68c338d68a93feca98a29b5a6c',1,'navmap::AABB']]],
-  ['max_5fedge_5flen_6',['max_edge_len',['../structnavmap__ros_1_1BuildParams.html#ad13e370af216e1188c1268b74472f57f',1,'navmap_ros::BuildParams']]],
-  ['max_5fslope_5fdeg_7',['max_slope_deg',['../structnavmap__ros_1_1BuildParams.html#a0ff349dd9350a87c621e936084ec2d1f',1,'navmap_ros::BuildParams']]],
-  ['messages_8',['8. Serialize to / from ROS messages',['../index.html#autotoc_md26',1,'']]],
-  ['min_9',['min',['../structnavmap_1_1AABB.html#a281c95909b643b5abda2fdd06e51ba30',1,'navmap::AABB']]],
-  ['min_5fangle_5fdeg_10',['min_angle_deg',['../structnavmap__ros_1_1BuildParams.html#a7652613347524c6ec775068fd6c24ca9',1,'navmap_ros::BuildParams']]],
-  ['min_5farea_11',['min_area',['../structnavmap__ros_1_1BuildParams.html#ab6746f896b14d17115f0a020de06c4a7',1,'navmap_ros::BuildParams']]],
-  ['minimal_20navmap_20single_20square_20floor_20with_20two_20triangles_12',['1. Create a minimal NavMap (single square floor with two triangles)',['../index.html#autotoc_md12',1,'']]],
-  ['mutable_5fdata_13',['mutable_data',['../structnavmap_1_1LayerView.html#a9a056707321fa283000ca974d30da70f',1,'navmap::LayerView::mutable_data()'],['../structLayerView.html#a9a056707321fa283000ca974d30da70f',1,'LayerView::mutable_data()']]]
+  ['max_5fdz_6',['max_dz',['../structnavmap__ros_1_1BuildParams.html#a8c2901c184925044a9bd4e62b6b0a79b',1,'navmap_ros::BuildParams']]],
+  ['max_5fedge_5flen_7',['max_edge_len',['../structnavmap__ros_1_1BuildParams.html#ad13e370af216e1188c1268b74472f57f',1,'navmap_ros::BuildParams']]],
+  ['max_5fslope_5fdeg_8',['max_slope_deg',['../structnavmap__ros_1_1BuildParams.html#a0ff349dd9350a87c621e936084ec2d1f',1,'navmap_ros::BuildParams']]],
+  ['messages_9',['8. Serialize to / from ROS messages',['../index.html#autotoc_md26',1,'']]],
+  ['min_10',['min',['../structnavmap_1_1AABB.html#a281c95909b643b5abda2fdd06e51ba30',1,'navmap::AABB']]],
+  ['min_5fangle_5fdeg_11',['min_angle_deg',['../structnavmap__ros_1_1BuildParams.html#a7652613347524c6ec775068fd6c24ca9',1,'navmap_ros::BuildParams']]],
+  ['min_5farea_12',['min_area',['../structnavmap__ros_1_1BuildParams.html#ab6746f896b14d17115f0a020de06c4a7',1,'navmap_ros::BuildParams']]],
+  ['minimal_20navmap_20single_20square_20floor_20with_20two_20triangles_13',['1. Create a minimal NavMap (single square floor with two triangles)',['../index.html#autotoc_md12',1,'']]],
+  ['mutable_5fdata_14',['mutable_data',['../structnavmap_1_1LayerView.html#a9a056707321fa283000ca974d30da70f',1,'navmap::LayerView::mutable_data()'],['../structLayerView.html#a9a056707321fa283000ca974d30da70f',1,'LayerView::mutable_data()']]]
 ];
