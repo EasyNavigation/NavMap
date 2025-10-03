87da4194984a4baa0903144eb391cfe902425171
fmrico <fmrico@users.noreply.github.com>
2025-10-03 06:05:44 +0000

    deploy: b56ca96c8a496e2af34cc3647a78ce681145789f

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['closest_5fpoint_5fon_5ftriangle_0',['closest_point_on_triangle',['../namespacenavmap.html#a8ec46dd39ad14c2364207e40c31f6764',1,'navmap']]],
-  ['closest_5ftriangle_1',['closest_triangle',['../classnavmap_1_1NavMap.html#af57e1de5f4e5a062fa37d1eddf9a2ed4',1,'navmap::NavMap::closest_triangle()'],['../classNavMap.html#af57e1de5f4e5a062fa37d1eddf9a2ed4',1,'NavMap::closest_triangle()']]],
+  ['closest_5fnavcel_0',['closest_navcel',['../classnavmap_1_1NavMap.html#aa1faa9583317df79ce4a24c92c68dc2f',1,'navmap::NavMap::closest_navcel()'],['../classNavMap.html#aa1faa9583317df79ce4a24c92c68dc2f',1,'NavMap::closest_navcel()']]],
+  ['closest_5fpoint_5fon_5ftriangle_1',['closest_point_on_triangle',['../namespacenavmap.html#a8ec46dd39ad14c2364207e40c31f6764',1,'navmap']]],
   ['contains_5fxy_2',['contains_xy',['../structnavmap_1_1AABB.html#a104e413ff13240e1418d5e545eabca42',1,'navmap::AABB']]],
   ['contains_5fxy_5fonly_3',['contains_xy_only',['../structnavmap_1_1AABB.html#a8548f0f32e8f29e0bdd2c53a40a6a3bb',1,'navmap::AABB']]],
   ['content_5fhash_4',['content_hash',['../structnavmap_1_1LayerViewBase.html#a7013c93c5b8c980840c8fb0138b3bf51',1,'navmap::LayerViewBase::content_hash()'],['../structnavmap_1_1LayerView.html#a0004705aea04a4d5ae569f630378a999',1,'navmap::LayerView::content_hash()'],['../structLayerView.html#a0004705aea04a4d5ae569f630378a999',1,'LayerView::content_hash()']]],
