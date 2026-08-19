a84f1f0fcf8f99325b01f7bd909ac2a87da7e043
fmrico <fmrico@users.noreply.github.com>
2026-08-19 08:33:30 +0000

    deploy: 9c7ae214e1f274a309513bc2514e4d14e2712836

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['expand_0',['expand',['../structnavmap_1_1AABB.html#aed36fd3d1b6ea1fe5eaf2fbabebb7465',1,'navmap::AABB::expand(const Vec3 &amp;p)'],['../structnavmap_1_1AABB.html#abf78f70737a4a8fa44e26bf01f422dcc',1,'navmap::AABB::expand(const AABB &amp;b)']]]
+  ['effective_5fmax_5fedge_5flen_0',['effective_max_edge_len',['../namespacenavmap__ros.html#ab45a2264fff18e80f39046cbd88f5911',1,'navmap_ros']]],
+  ['expand_1',['expand',['../structnavmap_1_1AABB.html#aed36fd3d1b6ea1fe5eaf2fbabebb7465',1,'navmap::AABB::expand(const Vec3 &amp;p)'],['../structnavmap_1_1AABB.html#abf78f70737a4a8fa44e26bf01f422dcc',1,'navmap::AABB::expand(const AABB &amp;b)']]]
 ];
